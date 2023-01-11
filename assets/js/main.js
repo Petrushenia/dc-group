119591b8b0fb2a4c9609cf63cbec5e7ecece3ee4
Petrushenia <sergworkyourself@gmail.com>
2023-01-11 21:47:27 +0300

    feat: add move circle

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,4 +48,70 @@ class Burger {
   }
 }
 
-const burger = new Burger(document.querySelector('.header'))
+class MouseCircle {
+  constructor(circle) {
+    this.circle = circle;
+    this.body = document.body;
+    this.mousePosY;
+    this.mousePosX;
+
+    this.circleLower = document.querySelectorAll('.lower');
+
+    this.body.addEventListener('mousemove', (e) => {
+      this.move(e);
+    })
+
+    window.addEventListener('scroll', () => {
+      this.savePos()
+    })
+
+    this.addListenerOver();
+    this.addListenerOut();;
+  }
+
+  move = (e) => {
+    this.getPageX(e);
+    this.getPageY(e);
+    this.circle.style.top = `${this.getPageY(e) - (this.circle.offsetWidth / 2)}px`;
+    this.circle.style.left = `${this.getPageX(e) - (this.circle.offsetWidth / 2)}px`;
+  }
+
+  getLower = () => {
+    this.circle.style.width = '12px';
+    this.circle.style.height = '12px';
+  }
+
+  getPageY = (e) => {
+    this.mousePosY = e.clientY;
+    return e.pageY;
+  }
+
+  getPageX = (e) => { 
+    this.mousePosX = e.clientX
+    return e.pageX;
+  }
+
+  savePos = () => {
+    this.circle.style.top = `${Math.abs((pageYOffset + this.mousePosY) - (this.circle.offsetWidth / 2))}px`
+  }
+
+  getBigger = () => {
+    this.circle.style.width = '26px';
+    this.circle.style.height = '26px';
+  }
+
+  addListenerOver = (e) => {
+    this.circleLower.forEach(element => {
+      element.addEventListener('mouseover', this.getLower)
+    })
+  }
+
+  addListenerOut = () => {
+    this.circleLower.forEach(element => {
+      element.addEventListener('mouseout', this.getBigger)
+    })
+  }
+}
+
+const burger = new Burger(document.querySelector('.header'));
+const mouseCircle = new MouseCircle(document.querySelector('.mouse-circle'));
