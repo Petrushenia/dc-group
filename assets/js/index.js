e572281b068068ee55afb455768db14fb3b10a04
Petrushenia <sergworkyourself@gmail.com>
2023-04-26 11:47:34 +0300

    feat: finis form-modal

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,4 +3,5 @@ import { burger } from './layout/burger/burger.js';
 import { circle } from './layout/circle/circle.js';
 import { review } from './layout/review/review.js';
 import { btnUp } from './layout/btn-up/btn.js'
+import { formModal } from './layout/request/request.modal.js';
 
