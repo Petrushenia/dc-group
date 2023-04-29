f080e295e256b9764c11f1580376f02e9be9b0d1
Petrushenia <sergworkyourself@gmail.com>
2023-04-29 21:01:47 +0300

    fix: modal form and burger

--- assets/js/layout/request/request.modal.js
+++ assets/js/layout/request/request.modal.js
@@ -2,28 +2,36 @@ class RequestModal {
   constructor(root) {
     this.root = root;
     this.constainer = this.root.querySelector('.modal-form-container');
-    this.closeBtn = this.constainer.querySelector('.close-menu')
-
+    this.closeBtn = this.constainer.querySelector('.close-menu');
     this.openButtons = document.querySelectorAll('.contact-button-js');
 
     this.openButtons.forEach(button => {
-      button.addEventListener('click', this.showFormModal);
+      button.addEventListener('click', this.openModal);
     })
 
     this.root.addEventListener('click', (e) => {
       if (e.target == this.constainer) {
-        this.showFormModal();
+        this.closeModal();
       }
     })
 
-    this.closeBtn.addEventListener('click', this.showFormModal);
+    this.closeBtn.addEventListener('click', this.closeModal);
 
   }
 
-  showFormModal = () => {
-    this.root.classList.toggle('modal-form-visible');
-    document.body.classList.toggle('disscroll');
+  openModal = () => {
+    this.root.classList.add('modal-form-visible');
+    document.body.classList.add('disscroll');
   }
+
+  closeModal = () => {
+    this.root.classList.remove('modal-form-visible');
+    document.body.classList.remove('disscroll');
+  }
+
+  
+
+  
 
 }
 
